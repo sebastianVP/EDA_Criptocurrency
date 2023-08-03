b9b2e43aa91445c9d04ab9c336d6a30799d9f76e
Alexander Valdez <alex.valdezp22@gmail.com>
2023-08-03 15:08:08 -0500

    last update se añadio download y df

--- criptocurrency-app.py
+++ criptocurrency-app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from PIL import Image
+import base64
 from bs4 import BeautifulSoup
 import  requests
 import json
+import pandas as pd
 
 #-----------------------------------#
 # Page Layout
@@ -42,29 +44,79 @@ currency_price_unit = col1.selectbox('Select currency for price',('USD','BTC','E
 @st.cache_data
 def load_data():
     cmc       = requests.get("https://coinmarketcap.com")
-    soup      = BeautifulSoul(cmc.content,'html.parser')
+    soup      = BeautifulSoup(cmc.content,'html.parser')
     data      = soup.find('script',id='__NEXT_DATA__',type='application/json')
     coin_data = json.loads(data.contents[0])
     listingsT = json.loads(coin_data['props']['initialState'])['cryptocurrency']['listingLatest']['data']
-    keysArr   = listingsT[0]
-    listings  = listngsT[1:]
+    keysArr   = listingsT[0]['keysArr']
+    listings  = listingsT[1:]
     coins     = {}
     for i in listings:
        coins[str(i[keysArr.index('id')])] = i[keysArr.index('slug')]
     coin_name   =[]
     coin_symbol =[]
-    maket_cap   =[]
+    market_cap   =[]
     percent_change_1h  = []
     percent_change_24h = []
     percent_change_7d  = []
     price              = []
-    volumen_24h        = []
+    volume_24h        = []
 
     for i in listings:
        coin_name.append(i[keysArr.index('slug')]) 
        coin_symbol.append(i[keysArr.index('symbol')])
-    #return df
+       price.append(i[keysArr.index('quote'+'.'+currency_price_unit+'.'+'price')])
+       percent_change_1h.append(i[keysArr.index('quote'+'.'+currency_price_unit+'.'+'percentChange1h')])
+       percent_change_24h.append(i[keysArr.index('quote'+'.'+currency_price_unit+'.'+'percentChange24h')])
+       percent_change_7d.append(i[keysArr.index('quote'+'.'+currency_price_unit+'.'+'percentChange7d')])
+       market_cap.append(i[keysArr.index('quote'+'.'+currency_price_unit+'.'+'marketCap')])
+       volume_24h.append(i[keysArr.index('quote'+'.'+currency_price_unit+'.'+'volume24h')])
 
-#df = load_data()
+    df = pd.DataFrame(columns=['coin_name', 'coin_symbol', 'market_cap', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d', 'price', 'volume_24h'])
+    df['coin_name'] = coin_name
+    df['coin_symbol'] = coin_symbol
+    df['price'] = price
+    df['percent_change_1h'] = percent_change_1h
+    df['percent_change_24h'] = percent_change_24h
+    df['percent_change_7d'] = percent_change_7d
+    df['market_cap'] = market_cap
+    df['volume_24h'] = volume_24h
+    return df
 
-## SIDEBAR - CRYPTOCURRENCY SELECTIONS
+df = load_data()
+
+## Sidebar - Cryptocurrency selections
+sorted_coin = sorted( df['coin_symbol'] )
+selected_coin = col1.multiselect('Cryptocurrency', sorted_coin, sorted_coin)
+
+df_selected_coin = df[ (df['coin_symbol'].isin(selected_coin)) ] # Filtering data
+## SIDEBAR - NUMBER OF COINS TO DISPLAY
+num_coin = col1.slider('Display Top N Coins', 1, 100, 100)
+df_coins = df_selected_coin[:num_coin]
+
+## Sidebar - Percent change timeframe
+percent_timeframe = col1.selectbox('Percent change time frame',
+                                    ['7d','24h', '1h'])
+percent_dict = {"7d":'percent_change_7d',"24h":'percent_change_24h',"1h":'percent_change_1h'}
+selected_percent_timeframe = percent_dict[percent_timeframe]
+
+## Sidebar - Sorting values
+sort_values = col1.selectbox('Sort values?', ['Yes', 'No'])
+
+col2.subheader('Price Data of Selected Cryptocurrency')
+col2.write('Data Dimension: ' + str(df_selected_coin.shape[0]) + ' rows and ' + str(df_selected_coin.shape[1]) + ' columns.')
+
+col2.dataframe(df_coins)
+
+# Download CSV Data
+def filedownload(df):
+     csv = df.to_csv(index=False)
+     b64 = base64.b64encode(csv.encode()).decode() # strings <-> bytes conversion
+     href = f'<a href="data:file/csv;base64,{b64}" download="crypto.csv">Download CSV</a>'
+     return href
+
+col2.markdown(filedownload(df_selected_coin),unsafe_allow_html=True)
+
+
+#-------------------------------#
+#Preparing data for BAR Plot of % Price Change
